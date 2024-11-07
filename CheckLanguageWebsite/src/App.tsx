25adf6f5d8cf65385388779ef22e88cbff605b60
Thiago Baptista <thiagobapt2003@gmail.com>
2024-11-07 16:57:25 -0300

    Started website

---
+++ CheckLanguageWebsite/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen flex place-content-center">
+        <div className="w-4/5">
+          <textarea className="h-full bg-transparent focus:outline-none w-full"></textarea>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

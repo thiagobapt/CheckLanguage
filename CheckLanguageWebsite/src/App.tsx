346f145f6f02230366032b41c1d7a04bfb36567c
Thiago Baptista <thiagobapt2003@gmail.com>
2024-11-08 02:39:56 -0300

    Iniciando site e parser para o editor

--- CheckLanguageWebsite/src/App.tsx
+++ CheckLanguageWebsite/src/App.tsx
@@ -1,12 +1,60 @@
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { useState } from "react";
+import {LRLanguage} from "@codemirror/language"
+import {completeFromList} from "@codemirror/autocomplete"
+import {parser} from "./CheckLang"
+import {foldNodeProp, foldInside, indentNodeProp} from "@codemirror/language"
+import {styleTags, tags as t} from "@lezer/highlight"
+import {LanguageSupport} from "@codemirror/language"
+
+let parserWithMetadata = parser.configure({
+  props: [
+    styleTags({
+      Name: t.variableName,
+      Number: t.number,
+      Boolean: t.bool,
+      String: t.string,
+      Initialization: t.definition(t.atom),
+      Assignment: t.definition(t.atom),
+      If_Statement: t.controlKeyword,
+      Else_Statement: t.controlKeyword,
+      Function_Call: t.bool,
+      "( )": t.paren,
+      "{ }": t.bracket,
+      "=": t.operator
+    }),
+    indentNodeProp.add({
+      Application: context => context.column(context.node.from) + context.unit
+    }),
+    foldNodeProp.add({
+      Application: foldInside
+    })
+  ]
+})
+
+const checkLanguage = LRLanguage.define({
+  parser: parserWithMetadata
+})
+
+const checkCompletion = checkLanguage.data.of({
+  autocomplete: completeFromList([
+    {label: "var", type: "keyword"},
+  ])
+})
+
+function check() {
+  return new LanguageSupport(checkLanguage, [checkCompletion])
+}
 
 function App() {
 
+  const [text, setText] = useState("");
+
   return (
     <>
-      <div className="w-screen h-screen flex place-content-center">
-        <div className="w-4/5">
-          <textarea className="h-full bg-transparent focus:outline-none w-full"></textarea>
-        </div>
+      <div className="w-full h-screen">
+        <ReactCodeMirror height="full" value={text} theme={vscodeDark} extensions={[check()]}/>
       </div>
     </>
   )
